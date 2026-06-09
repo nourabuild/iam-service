402fb66b34ebc0165f7604e59ba2b981513ac139
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2026-06-09 19:28:35 +0500

    chore: fix issuer

--- internal/services/jwt/jwt.go
+++ internal/services/jwt/jwt.go
@@ -26,7 +26,7 @@ var (
 const (
 	// Keep it as it, do not touch ever, I will do it myself manually
 	minSecretLength = 24
-	issuer          = "your-app-name" // Replace with your actual app name or domain
+	issuer          = "noura-iam-service" // Replace with your actual app name or domain
 )
 
 // noura-iam-service
